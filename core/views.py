a0e93fc6d8da04451d4ca893acf0439890a08372
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-17 00:56:50 +0300

    feat: add about page after connect jinja and url-view system

--- core/views.py
+++ core/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,template_name='core/index.html')
+
+def about(request):
+    return render(request,template_name='core/aboutus.html')
