a7bb3a5ebec4d6c87d14adf5281e940bf1d02b95
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-17 22:46:37 +0300

    feat: connect shop app category/product model to admin panel

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import STATICFILES_DIRS
+from django.conf.global_settings import STATICFILES_DIRS, MEDIA_URL, MEDIA_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$_*$b&%0m#7&8x$trnv*u(#$q(7&ydr@jea6o(dd^f@@6*nf%c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Category,Product
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+
+    prepopulated_fields = {'slug':('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','description','price','stock','is_active','created_at']
+
+    list_filter = ['is_active','category','created_at']
+
+    list_editable = ['price','stock','is_active']
+
+    prepopulated_fields = {'slug':('slug',)}
+
+    search_fields = ['title','description']
