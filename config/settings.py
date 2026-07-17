fb8d1a0fdf66b66cfe26c086bdaaa371621d5665
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-17 15:47:50 +0300

    chore: update settings.py and add shop/card apps

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
+    'shop.apps.ShopConfig',
+    'card.apps.CardConfig'
 ]
 
 MIDDLEWARE = [
